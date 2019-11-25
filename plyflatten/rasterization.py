8e9c27cc9062d9caaf342e3690b80e9a25a4d784
Guillaume Lostis <g.lostis@kayrros.com>
2019-11-25 12:04:21 +0100

    Blackify existing code

--- plyflatten/rasterization.py
+++ plyflatten/rasterization.py
@@ -1,31 +1,26 @@
 # Copyright (C) 2019, David Youssefi (CNES) <david.youssefi@cnes.fr>
 
 
-import re
-import os
-import numpy as np
 import ctypes
-from numpy.ctypeslib import ndpointer
+import os
+import re
 
 import affine
-
+import numpy as np
+from numpy.ctypeslib import ndpointer
 from s2p import geographiclib, ply
 
 # TODO: This is kind of ugly. Cleaner way to do this is to update
 # LD_LIBRARY_PATH, which we should do once we have a proper config file
 parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-lib = ctypes.CDLL(os.path.join(parent_dir, 'lib', 'libplyflatten.so'))
+lib = ctypes.CDLL(os.path.join(parent_dir, "lib", "libplyflatten.so"))
 
 
 class InvalidPlyCommentsError(Exception):
     pass
 
 
-def plyflatten(cloud,
-               xoff, yoff,
-               resolution,
-               xsize, ysize,
-               radius, sigma):
+def plyflatten(cloud, xoff, yoff, resolution, xsize, ysize, radius, sigma):
     """
     Projects a points cloud into the raster band(s) of a raster image
 
@@ -50,27 +45,35 @@ def plyflatten(cloud,
     raster_shape = (xsize * ysize, nb_extra_columns)
 
     # Set expected args and return types
-    lib.rasterize_cloud.argtypes = (ndpointer(dtype=ctypes.c_double,
-                                              shape=np.shape(cloud)),
-                                    ndpointer(dtype=ctypes.c_float,
-                                              shape=raster_shape),
-                                    ctypes.c_int,
-                                    ctypes.c_int,
-                                    ctypes.c_double, ctypes.c_double,
-                                    ctypes.c_double,
-                                    ctypes.c_int, ctypes.c_int,
-                                    ctypes.c_int, ctypes.c_float)
+    lib.rasterize_cloud.argtypes = (
+        ndpointer(dtype=ctypes.c_double, shape=np.shape(cloud)),
+        ndpointer(dtype=ctypes.c_float, shape=raster_shape),
+        ctypes.c_int,
+        ctypes.c_int,
+        ctypes.c_double,
+        ctypes.c_double,
+        ctypes.c_double,
+        ctypes.c_int,
+        ctypes.c_int,
+        ctypes.c_int,
+        ctypes.c_float,
+    )
 
     # Call rasterize_cloud function from libplyflatten.so
-    raster = np.zeros(raster_shape, dtype='float32')
-    lib.rasterize_cloud(np.ascontiguousarray(cloud.astype(np.float64)),
-                        raster,
-                        nb_points,
-                        nb_extra_columns,
-                        xoff, yoff,
-                        resolution,
-                        xsize, ysize,
-                        radius, sigma)
+    raster = np.zeros(raster_shape, dtype="float32")
+    lib.rasterize_cloud(
+        np.ascontiguousarray(cloud.astype(np.float64)),
+        raster,
+        nb_points,
+        nb_extra_columns,
+        xoff,
+        yoff,
+        resolution,
+        xsize,
+        ysize,
+        radius,
+        sigma,
+    )
 
     # Transform result into a numpy array
     raster = raster.reshape((ysize, xsize, nb_extra_columns))
@@ -118,20 +121,17 @@ def plyflatten_from_plyfiles_list(clouds_list, resolution, radius=0, roi=None, s
     # The copy() method will reorder to C-contiguous order by default:
     full_cloud = full_cloud.copy()
     sigma = float("inf") if sigma is None else sigma
-    raster = plyflatten(full_cloud, xoff, yoff, resolution,
-                        xsize, ysize,
-                        radius, sigma)
+    raster = plyflatten(full_cloud, xoff, yoff, resolution, xsize, ysize, radius, sigma)
 
     utm_zone = utm_zone_from_ply(clouds_list[0])
     utm_proj = geographiclib.utm_proj(utm_zone)
 
     # construct profile dict
     profile = dict()
-    profile['tiled'] = True
-    profile['nodata'] = float('nan')
-    profile['crs'] = utm_proj.srs
-    profile['transform'] = affine.Affine(resolution, 0.0, xoff,
-                                         0.0, -resolution, yoff)
+    profile["tiled"] = True
+    profile["nodata"] = float("nan")
+    profile["crs"] = utm_proj.srs
+    profile["transform"] = affine.Affine(resolution, 0.0, xoff, 0.0, -resolution, yoff)
 
     return raster, profile
 
