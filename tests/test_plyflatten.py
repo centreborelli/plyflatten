fc7eaa4cd813f00ab550e71f640ab62a4348ee1a
Guillaume Lostis <g.lostis@kayrros.com>
2019-11-26 10:21:29 +0100

    Make real tests

--- tests/test_plyflatten.py
+++ tests/test_plyflatten.py
@@ -1,6 +1,7 @@
 # pylint: disable=redefined-outer-name, missing-docstring
 import os
 
+import numpy as np
 import pytest
 import rasterio
 
@@ -18,7 +19,26 @@ def clouds_list():
     return clouds_list
 
 
-def test_plyflatten_from_plyfiles_list(clouds_list):
-    raster, profile = plyflatten_from_plyfiles_list(clouds_list, resolution=1)
+@pytest.fixture()
+def expected_raster():
+    here = os.path.abspath(os.path.dirname(__file__))
 
-    assert 1
+    with rasterio.open(os.path.join(here, "data", "result.tiff")) as f:
+        raster = f.read(1)
+
+    return raster
+
+
+def test_plyflatten_from_plyfiles_list(clouds_list, expected_raster):
+    raster, _ = plyflatten_from_plyfiles_list(clouds_list, resolution=2)
+    raster = raster[:, :, 0]
+
+    np.testing.assert_allclose(expected_raster, raster, equal_nan=True)
+
+
+def test_resolution(clouds_list, expected_raster):
+    raster, _ = plyflatten_from_plyfiles_list(clouds_list, resolution=4)
+    raster = raster[:, :, 0]
+
+    assert raster.shape[0] == expected_raster.shape[0] / 2
+    assert raster.shape[1] == expected_raster.shape[1] / 2
